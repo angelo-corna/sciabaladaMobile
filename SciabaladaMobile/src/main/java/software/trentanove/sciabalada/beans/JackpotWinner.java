ca8317fb9a99c02c2fb04e0427c714942102d7bd
Angelo Corna <angelo_corna@it.ibm.com>
2024-01-27 00:56:30 +0100

    Montepremi

---
+++ SciabaladaMobile/src/main/java/software/trentanove/sciabalada/beans/JackpotWinner.java
@@ -0,0 +1,22 @@
+package software.trentanove.sciabalada.beans;
+
+public class JackpotWinner {  
+ 
+	private String winner;  
+	private double jackpot;
+	
+	public String getWinner() {
+		return winner;
+	}
+	public void setWinner(String winner) {
+		this.winner = winner;
+	}
+	public double getJackpot() {
+		return jackpot;
+	}
+	public void setJackpot(double jackpot) {
+		this.jackpot = jackpot;
+	}  
+
+
+}  
